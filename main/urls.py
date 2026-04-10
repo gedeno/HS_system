432d58b3b8c5df6911929f6865ce1357fbbc5215
gedeno <gedenogeremew@gmail.com>
2026-04-10 22:20:15 +0300

    all page is ended to ready work

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView ,LoginView,HomeView,Personal
+from .views import RegisterView ,LoginView,HomeView,Personal,Contact_address,Emergency_contact
 
 urlpatterns = [
     path('register/',RegisterView.as_view() , name='register'),
     path('logins/', LoginView.as_view() , name='logins'),
     path('',HomeView.as_view() , name="home"),
-    path('personal',Personal.as_view() , name = "personal"),
+    path('personal/',Personal.as_view() , name = 'personal'),
+    path('contactaddress/',Contact_address.as_view() , name= 'contactaddress'),
+    path('emergencycontact/',Emergency_contact.as_view(), name='emergencycontact' ),
 ]
