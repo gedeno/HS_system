d9ba360822f7b55cb9ad3f5f137506cda5c71863
gedeno <gedenogeremew@gmail.com>
2026-04-11 20:23:11 +0300

    small error are fixing

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Personal, Contact_address, Emergency_contact, Course, Assessment
 class PersonalForm(ModelForm):
     class Meta:
         model = Personal
         fields = '__all__'
         exclude = ['user']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+        }
+        
 class ContactAddressForm(ModelForm):
     class Meta:
         model = Contact_address
